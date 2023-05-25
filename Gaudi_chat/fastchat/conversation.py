712cd44f7d4d3122018b39b070c5e64063d23a3c
Greg Serochi <56761205+greg-serochi@users.noreply.github.com>
2023-05-25 13:42:47 -0700

    Add files via upload

---
+++ Gaudi_chat/fastchat/conversation.py
@@ -0,0 +1,238 @@
+import dataclasses
+from enum import auto, Enum
+from typing import List, Tuple, Any
+
+
+class SeparatorStyle(Enum):
+    """Different separator style."""
+    SINGLE = auto()
+    TWO = auto()
+    DOLLY = auto()
+
+
+@dataclasses.dataclass
+class Conversation:
+    """A class that keeps all conversation history."""
+    system: str
+    roles: List[str]
+    messages: List[List[str]]
+    offset: int
+    sep_style: SeparatorStyle = SeparatorStyle.SINGLE
+    sep: str = "###"
+    sep2: str = None
+
+    # Used for gradio server
+    skip_next: bool = False
+    conv_id: Any = None
+
+    def get_prompt(self):
+        if self.sep_style == SeparatorStyle.SINGLE:
+            ret = self.system
+            for role, message in self.messages:
+                if message:
+                    ret += self.sep + " " + role + ": " + message
+                else:
+                    ret += self.sep + " " + role + ":"
+            return ret
+        elif self.sep_style == SeparatorStyle.TWO:
+            seps = [self.sep, self.sep2]
+            ret = self.system + seps[0]
+            for i, (role, message) in enumerate(self.messages):
+                if message:
+                    ret += role + ": " + message + seps[i % 2]
+                else:
+                    ret += role + ":"
+            return ret
+        elif self.sep_style == SeparatorStyle.DOLLY:
+            seps = [self.sep, self.sep2]
+            ret = self.system
+            for i, (role, message) in enumerate(self.messages):
+                if message:
+                    ret += role + ":\n" + message + seps[i % 2]
+                    if i % 2 == 1:
+                        ret += "\n\n"
+                else:
+                    ret += role + ":\n"
+            return ret
+        else:
+            raise ValueError(f"Invalid style: {self.sep_style}")
+
+    def append_message(self, role, message):
+        self.messages.append([role, message])
+
+    def to_gradio_chatbot(self):
+        ret = []
+        for i, (role, msg) in enumerate(self.messages[self.offset:]):
+            if i % 2 == 0:
+                ret.append([msg, None])
+            else:
+                ret[-1][-1] = msg
+        return ret
+
+    def copy(self):
+        return Conversation(
+            system=self.system,
+            roles=self.roles,
+            messages=[[x, y] for x, y in self.messages],
+            offset=self.offset,
+            sep_style=self.sep_style,
+            sep=self.sep,
+            sep2=self.sep2,
+            conv_id=self.conv_id)
+
+    def dict(self):
+        return {
+            "system": self.system,
+            "roles": self.roles,
+            "messages": self.messages,
+            "offset": self.offset,
+            "sep": self.sep,
+            "sep2": self.sep2,
+            "conv_id": self.conv_id,
+        }
+
+
+conv_one_shot = Conversation(
+    system="A chat between a curious human and an artificial intelligence assistant. "
+           "The assistant gives helpful, detailed, and polite answers to the human's questions.",
+    roles=("Human", "Assistant"),
+    messages=(
+        ("Human", "What are the key differences between renewable and non-renewable energy sources?"),
+        ("Assistant",
+            "Renewable energy sources are those that can be replenished naturally in a relatively "
+            "short amount of time, such as solar, wind, hydro, geothermal, and biomass. "
+            "Non-renewable energy sources, on the other hand, are finite and will eventually be "
+            "depleted, such as coal, oil, and natural gas. Here are some key differences between "
+            "renewable and non-renewable energy sources:\n"
+            "1. Availability: Renewable energy sources are virtually inexhaustible, while non-renewable "
+            "energy sources are finite and will eventually run out.\n"
+            "2. Environmental impact: Renewable energy sources have a much lower environmental impact "
+            "than non-renewable sources, which can lead to air and water pollution, greenhouse gas emissions, "
+            "and other negative effects.\n"
+            "3. Cost: Renewable energy sources can be more expensive to initially set up, but they typically "
+            "have lower operational costs than non-renewable sources.\n"
+            "4. Reliability: Renewable energy sources are often more reliable and can be used in more remote "
+            "locations than non-renewable sources.\n"
+            "5. Flexibility: Renewable energy sources are often more flexible and can be adapted to different "
+            "situations and needs, while non-renewable sources are more rigid and inflexible.\n"
+            "6. Sustainability: Renewable energy sources are more sustainable over the long term, while "
+            "non-renewable sources are not, and their depletion can lead to economic and social instability.")
+    ),
+    offset=2,
+    sep_style=SeparatorStyle.SINGLE,
+    sep="###",
+)
+
+
+conv_vicuna_v1_1 = Conversation(
+    system="A chat between a curious user and an artificial intelligence assistant. "
+           "The assistant gives helpful, detailed, and polite answers to the user's questions.",
+    roles=("USER", "ASSISTANT"),
+    messages=(),
+    offset=0,
+    sep_style=SeparatorStyle.TWO,
+    sep=" ",
+    sep2="</s>",
+)
+
+
+conv_koala_v1 = Conversation(
+    system="BEGINNING OF CONVERSATION:",
+    roles=("USER", "GPT"),
+    messages=(),
+    offset=0,
+    sep_style=SeparatorStyle.TWO,
+    sep=" ",
+    sep2="</s>",
+)
+
+conv_dolly = Conversation(
+    system=
+    "Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n",
+    roles=('### Instruction', '### Response'),
+    messages=(),
+    offset=0,
+    sep_style=SeparatorStyle.DOLLY,
+    sep="\n\n",
+    sep2="### End",
+)
+
+conv_dolly2 = Conversation(
+    system=
+    "Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n",
+    roles=('### Instruction', '### Response'),
+    messages=(
+        ("### Instruction", "Can you make an itinerary for a 4 day trip to San Diego?"),
+        ("### Response", """
+Day 1:
+
+* Arrive in San Diego and check into your hotel
+* Visit the San Diego Zoo, which is home to a wide variety of animals from around the world, including pandas, koalas, and giraffes.
+* In the evening, head to the Gaslamp Quarter, a vibrant neighborhood filled with restaurants, bars, and live music venues.
+
+Day 2:
+
+* Start the day with a visit to Balboa Park, a large urban park that is home to a number of museums, gardens, and other attractions, including the San Diego Zoo and the San Diego Air & Space Museum.
+* After lunch, head to La Jolla, a picturesque coastal neighborhood that is home to a variety of shops, restaurants, and beaches. You can take a stroll along the beach or visit the La Jolla Cove, a popular spot for swimming and snorkeling.
+* In the evening, catch a show at the San Diego Repertory Theatre or the La Jolla Playhouse.
+
+Day 3:
+
+* Spend the day at the beach! San Diego is known for its beautiful beaches, and there are many to choose from, including Mission Beach, Pacific Beach, and Ocean Beach. You can spend the day soaking up the sun, swimming, surfing, or trying your hand at other water sports.
+* In the evening, head to the Little Italy neighborhood, which is known for its trendy restaurants, bars, and shops. You can grab dinner at one of the many Italian restaurants or enjoy a glass of wine at a local tasting room.
+
+Day 4:
+
+* Start the day with a visit to the USS Midway Museum, which is housed on a former aircraft carrier and features a number of exhibits and interactive displays that tell the story of the ship and its crew.
+* After lunch, head to Coronado Island, which is home to a number of attractions, including the Hotel del Coronado, a historic hotel that has hosted celebrities and presidents, and the Coronado Island Maritime Museum, which tells the story of the island's naval history.
+* In the evening,""")
+    ),
+    offset=2,
+    sep_style=SeparatorStyle.DOLLY,
+    sep="\n\n",
+    sep2="### End",
+)
+
+conv_dolly3 = Conversation(
+    system=
+    "Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n",
+    roles=('### Instruction', '### Response'),
+    messages=(
+        ("### Instruction", "Can you make an itinerary for a 4 day trip to San Diego?"),
+        ("### Response", """Sure, here are some ideas for a trip to San Diego:
+Day 1: Go to the coast and visit the San Diego naval base. The base is close to a number of beaches with sunbathing areas, and you can also visit the beach at San Onofre if you want a day at the beach away from the city.
+Day 2: Spend the day in the city visiting the San Diego Air and Space Museum, Maritime Museum, and Balboa Park.
+Day 3: Take a day trip to Los Angeles, about a 2 hour drive away. You can visit the city's museums and parks, and you can also see the Hollywood Sign and other landmarks.
+Day 4: Take a day trip to Santa Barbara, about a 2 hour drive away. You can visit the city's beaches and museums, and you can also see the San Marcos Bridge and University of California campus.
+You could also spend the night in San Francisco instead of Los Angeles or Santa Barbara, and then drive back to San Diego the next day. Overall, this would be a fun trip with plenty of options to spend time in the cities or on the beach, and you could even visit multiple national landmarks. 
+Please let me know if you have any other questions.""")
+    ),
+    offset=2,
+    sep_style=SeparatorStyle.DOLLY,
+    sep="\n\n",
+    sep2="### End",
+)
+
+conv_templates = {
+    "conv_one_shot": conv_one_shot,
+    "vicuna_v1.1": conv_vicuna_v1_1,
+    "koala_v1": conv_koala_v1,
+    "dolly": conv_dolly,
+    "dolly2": conv_dolly2,
+    "dolly3": conv_dolly3
+}
+
+
+def get_default_conv_template(model_name):
+    model_name = model_name.lower()
+    if "vicuna" in model_name or "output" in model_name:
+        return conv_vicuna_v1_1
+    elif "koala" in model_name:
+        return conv_koala_v1
+    elif "dolly" in model_name:
+        return conv_dolly
+    return conv_one_shot
+
+
+if __name__ == "__main__":
+    print(default_conversation.get_prompt())
---
+++ Gaudi_chat/fastchat/serve/inference.py
@@ -0,0 +1,362 @@
+# Copyright(C) 2023 Habana Labs, Ltd. an Intel Company
+"""Inference for FastChat models."""
+import abc
+from typing import Optional
+import warnings
+
+import torch
+import torch.nn.functional as F
+from transformers import AutoTokenizer, AutoModel, AutoConfig, AutoModelForCausalLM, AutoModelForSeq2SeqLM
+from fastchat.serve.modeling_gpt_neox_hpu import GPTNeoXForCausalLM
+
+from fastchat.conversation import conv_templates, get_default_conv_template, SeparatorStyle
+from fastchat.serve.compression import compress_module
+from fastchat.serve.monkey_patch_non_inplace import replace_llama_attn_with_non_inplace_operations
+from fastchat.serve.serve_chatglm import chatglm_generate_stream
+import time
+import numpy as np
+import os
+
+tps_message = ''
+token_count = 0
+
+def raise_warning_for_old_weights(model_path, model):
+    if "vicuna" in model_path.lower():
+        try:
+            is_vicuna = isinstance(model, LlamaForCausalLM)
+        except Exception:
+            is_vicuna = isinstance(model, LLamaForCausalLM)
+        if is_vicuna and model.model.vocab_size > 32000:
+            warnings.warn(
+                "\nYou are probably using the old Vicuna-v0 model, "
+                "which will generate unexpected results with the "
+                "current fschat.\nYou can try one of the following methods:\n"
+                "1. Upgrade your weights to the new Vicuna-v1.1: https://github.com/lm-sys/FastChat#vicuna-weights.\n"
+                "2. Use the old conversation template by `python3 -m fastchat.serve.cli --model-path /path/to/vicuna-v0 --conv-template conv_one_shot`\n"
+                "3. Downgrade fschat to fschat==0.1.10 (Not recommonded).\n")
+
+
+def get_gpu_memory(max_gpus=None):
+    gpu_memory = []
+    num_gpus = torch.cuda.device_count() if max_gpus is None else min(max_gpus, torch.cuda.device_count())
+    
+    for gpu_id in range(num_gpus):
+        with torch.cuda.device(gpu_id):
+            device = torch.cuda.current_device()
+            gpu_properties = torch.cuda.get_device_properties(device)
+            total_memory = gpu_properties.total_memory / (1024**3)
+            allocated_memory = torch.cuda.memory_allocated() / (1024**3)
+            available_memory = total_memory - allocated_memory
+            gpu_memory.append(available_memory)
+    return gpu_memory
+
+
+def compute_skip_echo_len(model_name, conv, prompt):
+    model_name = model_name.lower()
+    if "chatglm" in model_name:
+        skip_echo_len = len(conv.messages[-2][1]) + 1
+    elif "dolly" in model_name:
+        special_toks = ["### Instruction:", "### Response:", "### End"]
+        prompt_tmp = prompt
+        for tok in special_toks:
+            prompt_tmp = prompt_tmp.replace(tok, "")
+        skip_echo_len = len(prompt_tmp)
+    else:
+        skip_echo_len = len(prompt) + 1 - prompt.count("</s>") * 3
+    return skip_echo_len
+
+
+def load_model(model_path, device, num_gpus, max_gpu_memory=None,
+               load_8bit=False, debug=False, use_graphs=False):
+    if device == "cpu":
+        kwargs = {}
+    elif device == "cuda":
+        kwargs = {"torch_dtype": torch.bfloat16}
+        if num_gpus == "auto":
+            kwargs["device_map"] = "auto"
+        else:
+            num_gpus = int(num_gpus)
+            if num_gpus != 1:
+                kwargs["device_map"] = "auto"
+                if max_gpu_memory is None:
+                    available_gpu_memory = get_gpu_memory(num_gpus)
+                    kwargs["max_memory"] = {i: str(int(available_gpu_memory[i] * 0.85)) +
+                        "GiB" for i in range(num_gpus)}
+                else:
+                    kwargs["max_memory"] = {i: max_gpu_memory for i in range(num_gpus)}
+        print("init_kwargs", kwargs)
+    elif device == "mps":
+        kwargs = {"torch_dtype": torch.float16}
+        # Avoid bugs in mps backend by not using in-place operations.
+        replace_llama_attn_with_non_inplace_operations()
+    elif device == "hpu":
+        import habana_frameworks.torch.core as htcore
+        import habana_frameworks.torch.hpu.graphs as htgraphs
+        import habana_frameworks.torch as ht
+        kwargs = {"torch_dtype": torch.bfloat16}
+    else:
+        raise ValueError(f"Invalid device: {device}")
+
+    config = AutoConfig.from_pretrained(model_path)
+    if "chatglm" in model_path:
+        tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+        model = AutoModel.from_pretrained(model_path, trust_remote_code=True).half().to(device)
+    elif "dolly" in model_path:
+        kwargs.update({"torch_dtype": torch.bfloat16})
+        tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
+        # 50277 means "### End"
+        tokenizer.eos_token_id = 50277
+        model = GPTNeoXForCausalLM.from_pretrained(model_path, low_cpu_mem_usage=True, **kwargs)
+        model.config.pad_token_id = model.config.eos_token_id
+    elif config.model_type == "llama":
+        tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
+        model = LlamaForCausalLM.from_pretrained(model_path, low_cpu_mem_usage=True, **kwargs)
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
+        model = AutoModelForCausalLM.from_pretrained(model_path,
+            low_cpu_mem_usage=True, **kwargs)
+        raise_warning_for_old_weights(model_path, model)
+
+    if load_8bit:
+        compress_module(model, device)
+
+    if (device == "cuda" and num_gpus == 1) or device == "mps":
+        model.to(device)
+    elif device == "hpu":
+        if config.model_type not in ['llama', 'gpt_neox']:
+            raise NotImplementedError
+        model.to(device)
+        model = model.eval()
+        if use_graphs:
+            model = ht.hpu.wrap_in_hpu_graph(model)
+
+    if debug:
+        print(model)
+
+    return model, tokenizer
+
+def get_device(model):
+    if hasattr(model, 'device'):
+        return model.device
+    if hasattr(model, 'module'):
+        return model.module.device
+    assert False, 'Cannot extract device!'
+    return None
+
+def prepare_input(model, max_length, static_shapes=False, **model_args):
+    device = get_device(model)
+    if static_shapes:
+        input_ids = model_args['input_ids']
+        attention_mask = model_args['attention_mask']
+        cur_length = input_ids.shape[-1]
+        padding_length = max_length - cur_length
+        model_args['token_idx'] = torch.tensor(cur_length)
+        model_args['input_ids'] = F.pad(input_ids, (0, padding_length), value=model.config.pad_token_id)
+        model_args['attention_mask'] = F.pad(attention_mask, (0, padding_length), value=0)
+    if 'token_idx' in model_args:
+        model_args['token_idx'] = model_args['token_idx'].to(device)
+    model_args['input_ids'] = model_args['input_ids'].to(device)
+    model_args['attention_mask'] = model_args['attention_mask'].to(device)
+    model_args['use_cache'] = model_args.get('use_cache')
+    return model_args
+
+
+@torch.inference_mode()
+def generate_stream(model, tokenizer, params, device,
+                    context_len=2048, stream_interval=2, 
+                    use_cache=False, static_shapes=False,
+                    output_tps=False, seed=1):
+    prompt = params["prompt"]
+    l_prompt = len(prompt)
+    temperature = float(params.get("temperature", 1.0))
+    max_new_tokens = int(params.get("max_new_tokens", 256))
+    stop_str = params.get("stop", None)
+    if stop_str == tokenizer.eos_token:
+        stop_str = None
+    max_length=context_len
+    model_args = tokenizer(prompt, return_tensors="pt", max_length=max_length, truncation=True)
+    model_args = model_args.to(device)
+    input_ids = model_args.input_ids
+    output_ids = input_ids.tolist()[0]
+
+    global token_count
+    token_count = len(output_ids)
+
+    eos_generated = torch.zeros((input_ids.shape[-2],), dtype=torch.bool, device=input_ids.device)
+
+    model_args = prepare_input(model, max_length=max_length, static_shapes=static_shapes, **model_args)
+    model_args['use_cache'] = use_cache
+
+
+    if device == 'hpu':
+        import habana_frameworks.torch.core as htcore
+        import habana_frameworks.torch.hpu.random as htrandom
+        htrandom.manual_seed(seed)
+        htcore.mark_step()
+        g = None
+    else:
+        g = torch.Generator(device=device).manual_seed(seed)
+
+    t1 = time.time()
+    n = 0
+    
+    for i in range(max_new_tokens):
+        n+=1
+        out = model(**model_args)
+        logits = out.logits
+        past_key_values = out.past_key_values
+        if static_shapes and (i == 0 or not use_cache):
+            last_token_logits = logits[:,model_args['token_idx'] - 1,:]
+        else:
+            last_token_logits = logits[:,-1,:]
+
+        if device == "mps":
+            # Switch to CPU by avoiding some bugs in mps backend.
+            last_token_logits = last_token_logits.float().to("cpu")
+
+        if temperature < 1e-4:
+            token = int(torch.argmax(last_token_logits))
+        else:
+            probs = torch.softmax(last_token_logits / temperature, dim=-1)         
+            token = int(torch.multinomial(probs, num_samples=1, generator=g))
+
+        output_ids.append(token)
+
+        if token == tokenizer.eos_token_id:
+            stopped = True
+        else:
+            stopped = False
+
+        if i % stream_interval == 0 or i == max_new_tokens - 1 or stopped:
+            output = tokenizer.decode(output_ids, skip_special_tokens=True)
+            if stop_str:
+                pos = output.rfind(stop_str, l_prompt)
+                if pos != -1:
+                    output = output[:pos]
+                    stopped = True
+            yield output
+
+        if stopped:
+            break
+        
+        next_tokens = torch.logical_not(eos_generated) * token + eos_generated * model.config.pad_token_id
+        eos_generated.logical_or_(next_tokens.eq(model.config.eos_token_id))
+        next_tokens = next_tokens.unsqueeze(-1)
+
+        # set input_ids for next forward pass
+        if use_cache:
+            model_args['input_ids'] = next_tokens
+            model_args['past_key_values'] = past_key_values
+        elif static_shapes:
+            model_args['input_ids'].index_copy_(1, model_args['token_idx'], next_tokens)
+        else:
+            model_args['input_ids'] = torch.cat([model_args['input_ids'], next_tokens], dim=-1)
+
+        # set attention_mask and token_idx
+        if static_shapes:
+            model_args['attention_mask'].index_fill_(1, model_args['token_idx'], 1)
+            model_args['token_idx'].add_(1)
+        else:
+            model_args['attention_mask'] = F.pad(model_args['attention_mask'], (0, 1), value=1)          
+
+        if device == 'hpu':
+            htcore.mark_step()
+
+    t2 = time.time()
+    if output_tps:
+        total_time = round(t2-t1, 3)
+        global tps_message
+        tps_message = '-------------------------------------------\nTime: ' + str(total_time) + "\tTokens: " + str(n) + '\tTPS: ' + str(round(n/total_time, 2)) + '\n-------------------------------------------'
+
+    token_count += n
+
+    del past_key_values, model_args, out, logits, last_token_logits
+
+
+class ChatIO(abc.ABC):
+    @abc.abstractmethod
+    def prompt_for_input(self, role: str) -> str:
+        """Prompt for input from a role."""
+
+    @abc.abstractmethod
+    def prompt_for_output(self, role: str):
+        """Prompt for output from a role."""
+
+    @abc.abstractmethod
+    def stream_output(self, output_stream, skip_echo_len: int):
+        """Stream output."""
+
+
+def chat_loop(model_path: str, device: str, num_gpus: str,
+              max_gpu_memory: str, load_8bit: bool,
+              conv_template: Optional[str], temperature: float,
+              max_new_tokens: int, chatio: ChatIO,
+              debug: bool, use_graphs: bool, use_cache: bool,
+              static_shapes: bool, context_len: int, output_tps: bool,
+              seed: int):
+    # Model
+    model, tokenizer = load_model(model_path, device,
+        num_gpus, max_gpu_memory, load_8bit, debug, use_graphs)
+    is_chatglm = "chatglm" in str(type(model)).lower()
+
+    # Chat
+    if conv_template:
+        conv = conv_templates[conv_template].copy()
+    else:
+        conv = get_default_conv_template(model_path).copy()
+
+    while True:
+        try:
+            inp = chatio.prompt_for_input(conv.roles[0])
+        except EOFError:
+            inp = ""
+        if not inp:
+            print("exit...")
+            break
+        
+        if inp == 'clear':
+            conv = conv_templates[conv_template].copy() if conv_template else get_default_conv_template(model_path).copy()
+            os.system('cls' if os.name == 'nt' else 'clear')
+            continue
+        
+        conv.append_message(conv.roles[0], inp)
+        conv.append_message(conv.roles[1], None)
+
+        if is_chatglm:
+            prompt = conv.messages[conv.offset:]
+            generate_stream_func = chatglm_generate_stream
+        else:
+            generate_stream_func = generate_stream
+            prompt = conv.get_prompt()
+
+        skip_echo_len = compute_skip_echo_len(model_path, conv, prompt)
+
+        params = {
+            "model": model_path,
+            "prompt": prompt,
+            "temperature": temperature,
+            "max_new_tokens": max_new_tokens,
+            "stop": conv.sep if conv.sep_style == SeparatorStyle.SINGLE else conv.sep2,
+        }
+
+        chatio.prompt_for_output(conv.roles[1])
+        output_stream = generate_stream_func(model, tokenizer, params, device, 
+                                             use_cache=use_cache, static_shapes=static_shapes, 
+                                             context_len=context_len, output_tps=output_tps,
+                                             seed=seed)
+        outputs = chatio.stream_output(output_stream, skip_echo_len)
+        if output_tps:
+            print(tps_message)
+        # NOTE: strip is important to align with the training data.
+        conv.messages[-1][-1] = outputs.strip()
+
+        if debug:
+            print("\n", {"prompt": prompt, "outputs": outputs}, "\n")
+            print('Token count: ' + str(token_count))
+
+        if token_count + max_new_tokens > context_len:
+            print('\n\nThe session history has been cleared because the maximum context length was reached\n\n')
+            if conv_template:
+                conv = conv_templates[conv_template].copy()
+            else:
+                conv = get_default_conv_template(model_path).copy()
