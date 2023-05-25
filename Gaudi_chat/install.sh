c0004819321ba155894347f359b175dbf6923d1e
Greg Serochi <56761205+greg-serochi@users.noreply.github.com>
2023-05-25 14:14:59 -0700

    Update install.sh

--- Gaudi_chat/install.sh
+++ Gaudi_chat/install.sh
@@ -1,4 +1,3 @@
+# This script will ensure that the pip installer is updated and will install the requirements for FastChat listed in pyproject.toml
 pip3 install --upgrade pip
 pip3 install -e .
-
-
