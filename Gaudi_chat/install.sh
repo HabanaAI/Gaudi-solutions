e475df5e1beb2342be8a44cc0ee9f4dc610e32b7
Greg Serochi <56761205+greg-serochi@users.noreply.github.com>
2023-05-25 13:43:40 -0700

    Add files via upload

---
+++ Gaudi_chat/install.sh
@@ -0,0 +1,4 @@
+pip3 install --upgrade pip
+pip3 install -e .
+
+
---
+++ Gaudi_chat/run_gaudi.sh
@@ -0,0 +1,2 @@
+python -m fastchat.serve.cli --model-path databricks/dolly-v2-3b --device hpu  --use_cache --temperature 0.7 --use_graphs --static_shapes  --output_tps --seed 12345 --conv-template dolly
+
