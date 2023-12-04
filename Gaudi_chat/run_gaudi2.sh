b8051651006e2de965d9b135478fe3101e2d18cb
Greg Serochi <56761205+greg-serochi@users.noreply.github.com>
2023-12-04 14:43:12 -0800

    Update run_gaudi2.sh

--- Gaudi_chat/run_gaudi2.sh
+++ Gaudi_chat/run_gaudi2.sh
@@ -1,2 +1,2 @@
-python -m fastchat.serve.cli --model-path meta-llama/Llama-2-7b-chat-hf --device hpu --use_cache --temperature 0.7 --use_graphs --static_shapes --output_tps --conv-template llama2
+python -m fastchat.serve.cli --model-path meta-llama/Llama-2-13b-chat-hf --device hpu --use_cache --temperature 0.7 --use_graphs --static_shapes --output_tps --conv-template llama2
 
